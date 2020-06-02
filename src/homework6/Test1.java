661147ad07b899b2b1bc66f1bf738f2e00141c0e
Sergei <njbots@yandex.ru>
2020-06-03 02:56:11 +0300

    Homework5

---
+++ src/homework6/Test1.java
@@ -0,0 +1,47 @@
+package homework6;
+
+public class Test1 {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+    @RunWith(Parameterized.class)
+    public class Task1Test {
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {new int[]{1, 6, 5, 4, 2, 3, 3}, new int[]{2, 3, 3}},
+                    {new int[]{4, 3, 7, 5, 4, 4, 3, 2}, new int[]{3, 2}}
+            });
+        }
+
+        private int[] in;
+        private int[] out;
+
+        public Task1Test(int[] in, int[] out) {
+            this.in = in;
+            this.out = out;
+        }
+
+        private Task1 task1;
+
+        @Before
+        public void startTest() {
+            task1 = new Task1();
+        }
+
+        @Test1
+        public void testAfterLast4() {
+            Assert.assertArrayEquals(out, Task1.AfterLast4(in));
+        }
+
+        @Test1(expected = RuntimeException.class)
+        public void testAfterLast4Ex() {
+            Task1.AfterLast4(new int[]{0, 3, 7, 5, 0, 0, 3, 2});
+        }
+    }
+}
---
+++ src/homework6/Test2.java
@@ -0,0 +1,34 @@
+package homework6;
+
+public class Test2 {
+    @RunWith(Parameterized.class)
+    public class Task2Test {
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {new int[]{1, 1, 4, 4, 4, 1, 4, 1}, true},
+                    {new int[]{8, 8, 7, 7, 7, 3, 5, 1}, false},
+                    {new int[]{1, 4, 8, 7}, false}
+            });
+        }
+        private int[] in;
+        private boolean out;
+
+        public Task2Test(int[] in, boolean out) {
+            this.in = in;
+            this.out = out;
+        }
+
+        private Task2 task2;
+
+        @Before
+        public void startTest() {
+            task2 = new Task2();
+        }
+
+        @Test
+        public void testOnly1And4() {
+            Assert.assertEquals(Task2.Only1And4(in), out);
+        }
+    }
+}
